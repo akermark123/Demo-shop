f134ded72423b513b2012c77e29996b600d833c2
akermark123 <39726486+akermark123@users.noreply.github.com>
2018-05-29 11:51:24 +0200

    Create kemikalier.js

---
+++ kemikalier.js
@@ -0,0 +1,7 @@
+var apple = new function() {
+    this.type = "macintosh";
+    this.color = "red";
+    this.getInfo = function () {
+        return this.color + ' ' + this.type + ' apple';
+    };
+}
